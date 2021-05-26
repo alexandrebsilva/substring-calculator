a987b6f0e17af066b0eb683ddcb29f1be5f14c7a
Alexandre Borges <alexandre.bsilva55@gmail.com>
2021-05-26 04:15:06 -0300

    adding more tests

--- tests/substring-calculator.test.js
+++ tests/substring-calculator.test.js
@@ -5,3 +5,22 @@ test("First example, input:  'kincenvizh' - should return 53", (done) => {
   expect(result).toEqual(53);
   done();
 });
+
+test("second example, input:  'abcde' - should return 15", (done) => {
+  const result = substringCalculator("abcde");
+  expect(result).toEqual(15);
+  done();
+});
+
+test("My name as an input:  'alexandre' - should return 43 ", (done) => {
+  const result = substringCalculator("alexandre");
+  expect(result).toEqual(43);
+  done();
+});
+
+test("Random example, input:  'altbank' - should return 27 ", (done) => {
+  const result = substringCalculator("altbank");
+  console.log(result);
+  expect(result).toEqual(27);
+  done();
+});
